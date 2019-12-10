89b1099e206ea3da982155f74da0416ae509d8d3
Rendi Pradana <39892767+skate473@users.noreply.github.com>
2019-12-10 17:45:12 +0700

    change variable

--- minmax.js
+++ minmax.js
@@ -1,32 +1,30 @@
-const number = [10, 5, 4, 2, 2, 3, 5, 8, 9, 4];
-let countTerkecil = [];
-let countTerbesar = [];
-let terkecil;
-let terbesar;
+let number = [10, 5, 4, 2, 2, 3, 5, 8, 9, 4];
+let arrMin = [];
+let arrMax = [];
+let min;
+let max;
 
 const getMinMax = number => {
-  terkecil = number[0];
-  terbesar = number[0];
+  min = number[0];
+  max = number[0];
 
   for (let i = 0; i < number.length; i++) {
-    if (number[i] < terkecil) {
-      terkecil = number[i];
+    if (number[i] < min) {
+      min = number[i];
     }
 
-    if (number[i] > terbesar) {
-      terbesar = number[i];
+    if (number[i] > max) {
+      max = number[i];
     }
   }
 
   for (let i = 0; i < number.length; i++) {
-    if (number[i] === terkecil) {
-      countTerkecil.push(number[i]);
+    if (number[i] === min) {
+      arrMin.push(number[i]);
     }
 
-    if (number[i] === terbesar) {
-      countTerbesar.push(number[i]);
+    if (number[i] === max) {
+      arrMax.push(number[i]);
     }
   }
 };
-
-getMinMax(number);
